cb2a3ecdedbd156e3245668bec0648d1ceae581b
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2021-11-18 08:38:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const fname = document.getElementById('firstName');
+const lname = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const submit = document.querySelector('.claim');
+
+function isBlank() {
+    if(fname.value.length == ''){
+        document.getElementById('firstNameError').style.display;
+    } else if (lname.value.length == ''){
+        document.getElementById('lastNameError').style.display;
+    } else if (email.value.length == ''){
+        document.getElementById('emailError').style.display;
+    } else if (password.value.length == ''){
+        document.getElementById('passwordError').style.display;
+    }
+}
+
+submit.addEventListener("click", isBlank);
